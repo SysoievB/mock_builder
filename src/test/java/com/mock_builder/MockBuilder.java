ee97097e802ceffb8e08faebce2adc57d2f2e73d
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-06 05:30:33 +0200

    Added annotation

--- src/test/java/com/mock_builder/MockBuilder.java
+++ src/test/java/com/mock_builder/MockBuilder.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD,ElementType.PARAMETER})
+@Target({ElementType.METHOD})
 public @interface MockBuilder {
 
     String[] setTrue() default {};
